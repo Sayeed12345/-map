68e2f6d7c567491f4399239e9568bbdbaa1b34cb
SAYEED <90544352+Sayeed12345@users.noreply.github.com>
2022-05-24 10:17:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const arr = [5, 4, 6, 7, 32];
+function double(x) {
+    return x * 4;
+
+}
+const output = arr.map(double);
+console.log(output);
+
+
+
+
+
+
+
+<script type="text.js">
+    function addNode(){
+        var newP=document.createElement("p");
+    var textNode = document.createTextNode("
+    This is a text node");
+    newP.appendChild(textNode);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function  create() {
+        var count=o;
+    return{
+        increment: function () {
+        count++;}
+    print:function () {
+        console.log(count);
+                }
+            }
+        }
+    var c=create();
+    c.increment();
+    c.print();
